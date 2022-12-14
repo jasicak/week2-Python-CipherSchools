ef7111a7688f12b59fe4c6737a41cc35e90b683f
Jasica Kaur <119054829+jasicak@users.noreply.github.com>
2022-12-14 22:44:26 +0530

    Update lecture_85-86.py

--- lecture_85-86.py
+++ lecture_85-86.py
@@ -1,15 +1,15 @@
 numbers=[1, 2, 3, 4]
 print(numbers)
 
-words=["muskan", "sharma", "life"]
+words=["jasica", "kaur", "life"]
 print(words)
 
-mixed=[1, 2 , 3, "muskan", "lunch", 2.3, None]
+mixed=[1, 2 , 3, "jasica", "lunch", 2.3, None]
 print(mixed)
 print(numbers[2])
 print(words[:2])
 print(mixed[-1])
 mixed[1]='two'
 print(mixed)
-words.append('jyoti')
+words.append('pranjal')
 print(words)
