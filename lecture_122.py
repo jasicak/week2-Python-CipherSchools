26406eb93d9d20ba6b19b1eda8d12337ec7c71e2
Jasica Kaur <119054829+jasicak@users.noreply.github.com>
2022-12-14 22:40:20 +0530

    Update lecture_122.py

--- lecture_122.py
+++ lecture_122.py
@@ -1,4 +1,4 @@
 # more about get, two same keys in dictionaries
-user={'name' : 'Muskan', 'age' : 17, 'age' : 34}
+user={'name' : 'Jasica', 'age' : 18, 'age' : 34}
 # print(user.get('names', 'not found !'))
 print(user)
