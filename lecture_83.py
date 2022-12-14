99d190fcf4f2faa7e74753fc3926ae793da2178d
Jasica Kaur <119054829+jasicak@users.noreply.github.com>
2022-12-14 22:43:11 +0530

    Update lecture_83.py

--- lecture_83.py
+++ lecture_83.py
@@ -2,5 +2,5 @@ def user_info(first_name,last_name,age="none"):
     print(f"your first name is {first_name}")
     print(f"your last name is {last_name}")
     print(f"your age is {age}")
-user_info('muskan','sharma')
+user_info('jasica','kaur')
 #default_parameter = always assign it at last
